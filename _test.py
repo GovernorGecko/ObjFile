99d3e31521029d47857de6ba3e819f93926a6fe7
John Bunting <john.lee.bunting@gmail.com>
2021-07-10 23:27:36 -0500

    Initial

--- _test.py
+++ _test.py
@@ -3,7 +3,19 @@
 """
 
 from src.generator import Generator
+from src.MultiD.src.triangle import Triangle
+from src.MultiD.src.vector import Vector3
 
 g = Generator("test")
+
+t = Triangle(
+    [
+        Vector3(1.0, 1.0, 1.0),
+        Vector3(1.0, 1.0, 0.9),
+        Vector3(0.0, 1.0, 1.0)
+    ]
+)
+
+g.add_triangle(t)
 
 print(g)
--- src/generator.py
+++ src/generator.py
@@ -7,16 +7,29 @@ from .MultiD.src.triangle import Triangle
 
 class Generator():
 
-    __slots__ = ["__faces", "__name", "__positions"]
+    __slots__ = [
+        "__faces", "__name", "__normals", "__positions",
+        "__render_colors", "__render_normals", "__render_texcoords"
+        ]
 
     def __init__(self, name, normals=True, colors=False, texcoords=False):
 
         if not isinstance(name, str):
             raise ValueError("Name must be a string.")
+        elif (
+            not isinstance(normals, bool) or
+            not isinstance(colors, bool) or
+            not isinstance(texcoords, bool)
+        ):
+            raise ValueError("Normals, Colors, TexCoords must be bool.")
 
         self.__faces = []
         self.__name = name
+        self.__normals = []
         self.__positions = []
+        self.__render_colors = colors
+        self.__render_normals = normals
+        self.__render_texcoords = texcoords
 
     def __str__(self):
         """
@@ -35,7 +48,29 @@ class Generator():
         if not isinstance(triangle, Triangle):
             raise ValueError("Triangle must be of type Triangle")
 
-    def add_vertex_data(self, vertex_data):
-        """
-        """
+        # Set up our Face Data
+        face_data = []
+        for p in triangle.__positions:
+            if p in self.__positions:
+                face_data.append(
+                    self.__positions.index(p)
+                )
+            else:
+                self.__positions.append(p)
+                face_data.append(
+                    len(face_data)
+                )
         
+        """
+        self.__faces.append(len(self.__faces))
+        triangle.__positions
+
+        vertex_data = triangle.get_vertex_data(
+            positions=True,
+            normals=self.__render_normals,
+            colors=self.__render_colors,
+            texcoords=self.__render_texcoords
+        )
+
+        print(vertex_data)
+        """
